64fd0e775e431b9790c167aa0c8bcb2f243c89ae
Anima Tiwary <animatiwary002@gmail.com>
2024-02-12 02:11:14 +0530

    Adding project for Product Management

---
+++ CrudBackend/CrudBackend/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using CrudBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDataDbContext _productDataDbContext;
+        public ProductController(ProductDataDbContext productDataDbContext)
+        {
+            _productDataDbContext = productDataDbContext;
+        }
+        [HttpGet]
+        [Route("GetProducts")]
+        public async Task<IEnumerable<ProductData>> GetProducts()
+        {
+            return await _productDataDbContext.Products.ToListAsync();
+        }
+        [HttpPost]
+        [Route("AddProduct")]
+        public async Task<ProductData> AddProduct(ProductData objProduct)
+        {
+            _productDataDbContext.Products.Add(objProduct);
+            await _productDataDbContext.SaveChangesAsync();
+            return objProduct;
+        }
+        [HttpPatch]
+        [Route("UpdateProduct/{id}")]
+        public async Task<ProductData> UpdateProduct(ProductData objProduct)
+        {
+            _productDataDbContext.Entry(objProduct).State = EntityState.Modified;
+            await _productDataDbContext.SaveChangesAsync();
+            return objProduct;
+        }
+        [HttpDelete]
+        [Route("DeleteProduct/{id}")]
+        public bool DeleteProduct(int id)
+        {
+            bool a = false;
+            var product = _productDataDbContext.Products.Find(id);
+            if (product != null)
+            {
+                a = true;
+                _productDataDbContext.Entry(product).State = EntityState.Deleted;
+                _productDataDbContext.SaveChanges();
+            }
+            else
+            {
+                a = false;
+            }
+            return a;
+        }
+
+        }
+    }
---
+++ CrudBackend/CrudBackend/Models/ProductDataDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudBackend.Models
+{
+    public class ProductDataDbContext : DbContext
+    {
+        public ProductDataDbContext(DbContextOptions<ProductDataDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductData> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DUMMY; Initial Catalog=Products; User Id=DUMMYUSERNAME; password=DUMMYPASSWORD; TrustServerCertificate= True");
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
